5a85b3a82786b5c530bb30231bd43bff88934623
haydenpereira <143343663+haydenpereira@users.noreply.github.com>
2023-09-20 17:22:38 -0700

    Update Balloon_game.py

--- OOPS/Balloon_game.py
+++ OOPS/Balloon_game.py
@@ -7,6 +7,10 @@ red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
 white = (255,255,255)
+def text(msg,x,y,color,size):
+    font_object = pygame.font.SysFont('Lobster',size)
+    message_object = font_object.render(msg,False,color)
+    screen.blit(message_object,(x,y))
 class Balloon:
     radius = 30
     def __init__(self,x,y):
@@ -15,6 +19,7 @@ class Balloon:
         self.letter = chr(random.randint(97,122))
     def draw(self):
         pygame.draw.circle(screen,blue,(self.x,self.y),self.radius)
+        text(self.letter,self.x - 10,self.y - 20,white,50)
     def move(self):
         self.y -= 0.5 
         if self.y <= 0:
@@ -28,8 +33,11 @@ while True:
     for i in balloon_list:
         i.draw()
         i.move()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            quit()
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key ==  i.letter:
+                    balloon_list.remove(i)
+            if event.type == QUIT:
+                pygame.quit()
+                quit()
     pygame.display.update()
